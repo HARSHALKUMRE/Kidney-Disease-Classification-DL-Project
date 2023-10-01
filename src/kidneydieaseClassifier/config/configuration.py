593aabe73f589129df2f74620e45f3a130c7d74c
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-10-01 22:02:44 +0530

    prepare callbacks adde

--- src/kidneydieaseClassifier/config/configuration.py
+++ src/kidneydieaseClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from kidneydieaseClassifier.constants import *
 from kidneydieaseClassifier.utils.common import read_yaml, create_directories
-from kidneydieaseClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from kidneydieaseClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
 
 class ConfigurationManager:
 
@@ -44,5 +44,21 @@ class ConfigurationManager:
             params_weights=self.params.WEIGHTS,
             params_classes=self.params.CLASSES
         )
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir = Path(config.root_dir),
+            tensorboard_root_log_dir = Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath = Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
 
         return prepare_base_model_config
